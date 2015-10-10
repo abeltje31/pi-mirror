421344fe8a35e178c05d1abbc68c5fc36c387d80
Maarten Abeling <abeling.m@gmail.com>
2015-10-10 21:31:40 +0200

    Included current weather

--- control.js
+++ control.js
@@ -1,3 +1,41 @@
+jQuery.fn.updateWithText = function(text, speed)
+{
+	var dummy = $('<div/>').html(text);
+
+	if ($(this).html() != dummy.html())
+	{
+		$(this).fadeOut(speed/2, function() {
+			$(this).html(text);
+			$(this).fadeIn(speed/2, function() {
+				//done
+			});
+		});
+	}
+}
+
+jQuery.fn.outerHTML = function(s) {
+    return s
+        ? this.before(s).remove()
+        : jQuery("<p>").append(this.eq(0).clone()).html();
+};
+
+function roundVal(temp)
+{
+	return Math.round(temp * 10) / 10;
+}
+
+function kmh2beaufort(kmh)
+{
+	var speeds = [1, 5, 11, 19, 28, 38, 49, 61, 74, 88, 102, 117, 1000];
+	for (var beaufort in speeds) {
+		var speed = speeds[beaufort];
+		if (speed > kmh) {
+			return beaufort;
+		}
+	}
+	return 12;
+}
+
 jQuery(document).ready(function($) {
     (function checkVersion()
     {
@@ -13,4 +51,61 @@ jQuery(document).ready(function($) {
             checkVersion();
         }, 3000);
     })();
+	
+	(function updateCurrentWeather()
+	{
+		var iconTable = {
+			'01d':'wi-day-sunny',
+			'02d':'wi-day-cloudy',
+			'03d':'wi-cloudy',
+			'04d':'wi-cloudy-windy',
+			'09d':'wi-showers',
+			'10d':'wi-rain',
+			'11d':'wi-thunderstorm',
+			'13d':'wi-snow',
+			'50d':'wi-fog',
+			'01n':'wi-night-clear',
+			'02n':'wi-night-cloudy',
+			'03n':'wi-night-cloudy',
+			'04n':'wi-night-cloudy',
+			'09n':'wi-night-showers',
+			'10n':'wi-night-rain',
+			'11n':'wi-night-thunderstorm',
+			'13n':'wi-night-snow',
+			'50n':'wi-night-alt-cloudy-windy'
+		}
+
+
+		$.getJSON('http://api.openweathermap.org/data/2.5/weather', weatherParams, function(json, textStatus) {
+
+			var temp = roundVal(json.main.temp);
+			var temp_min = roundVal(json.main.temp_min);
+			var temp_max = roundVal(json.main.temp_max);
+
+			var wind = roundVal(json.wind.speed);
+
+			var iconClass = iconTable[json.weather[0].icon];
+			var icon = $('<span/>').addClass('icon').addClass('dimmed').addClass('wi').addClass(iconClass);
+			$('.temp').updateWithText(icon.outerHTML()+temp+'&deg;', 1000);
+
+			// var forecast = 'Min: '+temp_min+'&deg;, Max: '+temp_max+'&deg;';
+			// $('.forecast').updateWithText(forecast, 1000);
+
+			var now = new Date();
+			var sunrise = new Date(json.sys.sunrise*1000).toTimeString().substring(0,5);
+			var sunset = new Date(json.sys.sunset*1000).toTimeString().substring(0,5);
+
+			var windString = '<span class="wi wi-strong-wind xdimmed"></span> ' + kmh2beaufort(wind) ;
+			var sunString = '<span class="wi wi-sunrise xdimmed"></span> ' + sunrise;
+			if (json.sys.sunrise*1000 < now && json.sys.sunset*1000 > now) {
+				sunString = '<span class="wi wi-sunset xdimmed"></span> ' + sunset;
+			}
+
+			$('.windsun').updateWithText(windString + ' ' + sunString, 1000);
+		});
+
+		setTimeout(function() {
+			updateCurrentWeather();
+		}, 60000);
+	})();
 });
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="css/weather-icons.css">
     <script>
         var currentgitversion = "<?php echo trim(shell_exec('git rev-parse HEAD')) ?>";
     </script>
@@ -8,8 +9,13 @@
 
 <body>
 
+
 <script src="jquery.js"></script>
+<script src="config.js"></script>
 <script src="control.js"></script>
+
+<div class="windsun"></div>
+<div class="temp"></div>
 
 <?php
 echo "Hello world! This is the second try :)";
